f059223e595727fec4379aaea4eaf42f9adaa418
kevinkiklee <kevin.kik.lee@gmail.com>
2026-04-29 21:53:46 -0400

    style: biome format cspell config

--- cspell.config.cjs
+++ cspell.config.cjs
@@ -2,9 +2,7 @@ module.exports = {
   version: '0.2',
   language: 'en',
   words: [],
-  dictionaryDefinitions: [
-    { name: 'project-words', path: './cspell-words.txt', addWords: true },
-  ],
+  dictionaryDefinitions: [{ name: 'project-words', path: './cspell-words.txt', addWords: true }],
   dictionaries: ['project-words'],
   ignorePaths: ['dist', '.astro', 'node_modules', 'pnpm-lock.yaml'],
 };
